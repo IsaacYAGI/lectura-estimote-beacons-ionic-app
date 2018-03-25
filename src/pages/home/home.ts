c4759c1e3958cec536cd046ae38138283bbb9a5c
Isaac Yriarte <isaacyriarte@gmail.com>
2018-03-25 18:54:54 -0500

    Se muestran los beacons a medida que se van encontrando

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,14 +10,39 @@ export class HomePage {
   beaconData : any;
 
   constructor(private changeDetector: ChangeDetectorRef, public navCtrl: NavController, private platform: Platform) {
-
+    this.beaconData = [];
   }
 
   startScanningForBeacons(){
     this.platform.ready().then(() => {
       evothings.eddystone.startScan((data) => {
-        //alert(JSON.stringify(data));
-        this.beaconData = data;
+        //console.log("YAGI-" + JSON.stringify(data));
+        //Si no se encuentra el beacon leido por el dispositivo en la lista...
+        console.log("YAGI - data=" + JSON.stringify(data));
+        //var buscarBeacon = this.beaconData.find(element => element.address == data.address);
+        let buscarBeacon = null;
+        if (this.beaconData !== null){
+          buscarBeacon = this.beaconData.find(function(element){
+            console.log("YAGI-" + "Buscando... element vs data="+element.address + " === " + data.address);
+            return element.address === data.address
+          });
+          
+          if (buscarBeacon === undefined){
+            this.beaconData.push(data);
+          }
+
+        }else this.beaconData.push(data);
+        console.log("YAGI-" + "buscarBeacon="+JSON.stringify(buscarBeacon));
+        /*
+        if ( buscarBeacon !== undefined){
+
+          //Se agrega a la lista
+          this.beaconData.push(data);
+          console.log("YAGI-" + "Se agrego un nuevo beacon a la lista!");
+        }else{
+          console.log("YAGI-" + "Buscarbeacon="+JSON.stringify(buscarBeacon));
+        }
+        */
         setTimeout(() => {
           this.changeDetector.detectChanges();
         },
@@ -28,7 +53,7 @@ export class HomePage {
 
   stopScanningForBeacons(){
     evothings.eddystone.stopScan();
-    this.beaconData = null;
+    this.beaconData = [];
   }
 
 }
