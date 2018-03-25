caa24ea253e1505c16e8b6ec9e16b68841e6ff20
Isaac Yriarte <isaacyriarte@gmail.com>
2018-03-25 14:15:33 -0500

    Se agrega el codigo para leer los beacons y mostrarlos por consola.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Platform } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,16 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private platform: Platform) {
 
+  }
+
+  startScanningForBeacons(){
+    this.platform.ready().then(() => {
+      evothings.eddystone.startScan((beaconData) => {
+        alert(JSON.stringify(beaconData));
+      }, error => alert(JSON.stringify(error)));
+    });
   }
 
 }
